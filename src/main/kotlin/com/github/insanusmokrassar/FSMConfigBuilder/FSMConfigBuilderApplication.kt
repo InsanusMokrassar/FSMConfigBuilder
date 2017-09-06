f06da534b3f185345f680bb40f20219526d3bd42
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-09-06 21:29:39 +0600

    prepare to write logic

--- src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/FSMConfigBuilderApplication.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/FSMConfigBuilderApplication.kt
@@ -4,10 +4,16 @@ import com.github.insanusmokrassar.FSMConfigBuilder.utils.ScenesManager
 import javafx.application.Application
 import javafx.stage.Stage
 
-class FSMConfigBuilderApplication : Application() {
+class FSMConfigBuilderApplication(
+        private val title: String = "FSM Config Builder",
+        private val version: Float = 0.1F
+) : Application() {
     private var scenesManager: ScenesManager? = null
     override fun start(primaryStage: Stage?) {
-        scenesManager = ScenesManager(primaryStage!!)
-        scenesManager?.addScene(javaClass.classLoader.getResource("main.fxml"))
+        primaryStage?.let {
+            it.title = "$title $version"
+            scenesManager = ScenesManager(it)
+            scenesManager?.addScene(javaClass.classLoader.getResource("main.fxml"))
+        }
     }
 }

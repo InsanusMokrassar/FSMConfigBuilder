dcce7732a6d554264f1ecd3dc453700126e8019d
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-09-07 15:51:44 +0600

    completed most part of guis

--- src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/controllers/ConfigBuilder.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/controllers/ConfigBuilder.kt
@@ -1,16 +1,26 @@
 package com.github.insanusmokrassar.FSMConfigBuilder.controllers
 
 import com.github.insanusmokrassar.FSMConfigBuilder.models.StateRow
+import com.github.insanusmokrassar.IObjectKRealisations.JSONIObject
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.value.ObservableValue
 import javafx.collections.FXCollections.observableArrayList
 import javafx.collections.ObservableList
+import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.*
 import javafx.scene.control.cell.CheckBoxTableCell
 import javafx.scene.control.cell.PropertyValueFactory
+import javafx.scene.control.cell.TextFieldTableCell
 import javafx.util.Callback
+import javafx.util.StringConverter
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.URL
 import java.util.*
+import java.util.logging.Logger
+import kotlin.collections.ArrayList
 
 class ConfigBuilder : Initializable {
 
@@ -18,6 +28,9 @@ class ConfigBuilder : Initializable {
 
     @FXML
     var previewLabel: Label? = null
+
+    @FXML
+    var errorLabel: Label? = null
 
     @FXML
     var generateBtn: Button? = null
@@ -32,46 +45,145 @@ class ConfigBuilder : Initializable {
     var statesTableView: TableView<StateRow>? = null
 
     @FXML
-    var numColumn: TableColumn<TableView<StateRow>, Int>? = null
+    var numColumn: TableColumn<StateRow, Int>? = null
 
     @FXML
-    var acceptColumn: TableColumn<TableView<StateRow>, Boolean>? = null
+    var acceptColumn: TableColumn<StateRow, Boolean>? = null
 
     @FXML
-    var errorColumn: TableColumn<TableView<StateRow>, Boolean>? = null
+    var errorColumn: TableColumn<StateRow, Boolean>? = null
 
     @FXML
-    var returnColumn: TableColumn<TableView<StateRow>, Boolean>? = null
+    var stackColumn: TableColumn<StateRow, Boolean>? = null
 
     @FXML
-    var nextColumn: TableColumn<TableView<StateRow>, Int>? = null
+    var nextColumn: TableColumn<StateRow, Int>? = null
 
     @FXML
-    var regexColumn: TableColumn<TableView<StateRow>, String>? = null
+    var regexColumn: TableColumn<StateRow, String>? = null
 
     @FXML
-    var callbackColumn: TableColumn<TableView<StateRow>, String>? = null
+    var callbackColumn: TableColumn<StateRow, String>? = null
+
+    @FXML
+    var addRowBtn: Button? = null
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
 
         //Say me how to do better??? -----
-        numColumn?.cellValueFactory = PropertyValueFactory("numField")
-        acceptColumn?.cellValueFactory = PropertyValueFactory("acceptField")
-        errorColumn?.cellValueFactory = PropertyValueFactory("errorField")
-        returnColumn?.cellValueFactory = PropertyValueFactory("returnField")
-        nextColumn?.cellValueFactory = PropertyValueFactory("nextField")
-        regexColumn?.cellValueFactory = PropertyValueFactory("regexField")
-        callbackColumn?.cellValueFactory = PropertyValueFactory("callbackField")
+        numColumn?.cellValueFactory = Callback {
+            it.value.numProperty as? ObservableValue<Int>
+        }
+        nextColumn?.cellValueFactory = Callback {
+            it.value.nextProperty as? ObservableValue<Int>
+        }
+        nextColumn?.let {
+            it.cellFactory = Callback {
+                TextFieldTableCell<StateRow, Int>(
+                        object: StringConverter<Int>() {
+                            override fun toString(what: Int?): String {
+                                what?.let {
+                                    return it.toString()
+                                }
+                                return "null"
+                            }
+
+                            override fun fromString(string: String?): Int {
+                                string?.let {
+                                    try {
+                                        return it.toInt()
+                                    } catch (e: NumberFormatException) {
+                                        sendError("You have typed not integer")
+                                    }
+                                }
+                                return -1
+                            }
+
+                        }
+                )
+            }
+            it.onEditCommit = EventHandler {
+                it.rowValue.nextProperty.set(it.newValue)
+                checkConfigAndShowError()
+
+            }
+        }
+        regexColumn?.cellValueFactory = Callback {
+            it.value.regexProperty
+        }
+        callbackColumn?.cellValueFactory = Callback {
+            it.value.callbackProperty
+        }
+
+        acceptColumn?.cellFactory = Callback{
+            CheckBoxTableCell<StateRow, Boolean>( Callback{
+                statesList[it].acceptProperty
+            })
+        }
+        errorColumn?.cellFactory = Callback{
+            CheckBoxTableCell<StateRow, Boolean>( Callback{
+                statesList[it].errorProperty
+            })
+        }
+        stackColumn?.cellFactory = Callback{
+            CheckBoxTableCell<StateRow, Boolean>( Callback{
+                statesList[it].stackProperty
+            })
+        }
         //-------
 
         statesTableView?.items = statesList
-        val testState = StateRow(0)
-        statesList.add(testState)
-        Thread({
-            while(true) {
 
-                Thread.sleep(1000)
+        addRowBtn?.onAction = EventHandler {
+            if (!checkConfigAndShowError()) {
+                val newState = StateRow()
+                newState.numProperty.set(statesList.size)
+                statesList.add(newState)
             }
-        }).start()
+        }
+
+        generateBtn?.onAction = EventHandler {
+            if (!checkConfigAndShowError()) {
+                previewLabel?.text = generateConfig()
+                return@EventHandler
+            }
+        }
+    }
+
+    private fun generateConfig(): String {
+        val config = ArrayList<String>()
+        statesList.forEach {
+            config.add(it.toString())
+        }
+        return config.toString()
+    }
+
+    /**
+     * return true if was error
+     */
+    private fun checkConfigAndShowError(): Boolean {
+        val error = checkConfig()
+        error?.let {
+            errorLabel?.text = error
+            return true
+        }
+        return false
+    }
+
+    private fun checkConfig(): String? {
+        val buffer = StringBuilder()
+        statesList.forEach {
+            if (!it.isValid()) {
+                buffer.append("${statesList.indexOf(it)}\n")
+            }
+        }
+        return (if (buffer.isEmpty()) null else "Next states are invalid:\n$buffer")
+    }
+
+    private fun sendError(error: String) {
+        errorLabel?.let {
+            it.text = error
+        }
+        Logger.getGlobal().warning(error)
     }
 }
--- src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/models/StateRow.kt
+++ src/main/kotlin/com/github/insanusmokrassar/FSMConfigBuilder/models/StateRow.kt
@@ -1,14 +1,31 @@
 package com.github.insanusmokrassar.FSMConfigBuilder.models
 
 import javafx.beans.property.SimpleBooleanProperty
-import javafx.beans.value.ObservableValue
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
 
 data class StateRow(
-        var numField: Int,
-        var acceptField: SimpleBooleanProperty = SimpleBooleanProperty(),
-        var errorField: Boolean = false,
-        var returnField: Boolean = false,
-        var nextField: Int? = null,
-        var regexField: String = "",
-        var callbackField: String? = null
-)
+        var numProperty: SimpleIntegerProperty = SimpleIntegerProperty(-1),
+        var acceptProperty: SimpleBooleanProperty = SimpleBooleanProperty(),
+        var errorProperty: SimpleBooleanProperty = SimpleBooleanProperty(),
+        var stackProperty: SimpleBooleanProperty = SimpleBooleanProperty(),
+        var nextProperty: SimpleIntegerProperty = SimpleIntegerProperty(-1),
+        var regexProperty: SimpleStringProperty = SimpleStringProperty("."),
+        var callbackProperty: SimpleStringProperty = SimpleStringProperty()
+) {
+    fun isValid(): Boolean {
+        return numProperty.get() >= 0
+                && !(nextProperty.get() == -1 && stackProperty.get())
+    }
+
+    override fun toString(): String {
+        return "[" +
+                "${acceptProperty.get()}," +
+                "${errorProperty.get()}," +
+                "${stackProperty.get()}," +
+                "${regexProperty.get()}," +
+                (if (nextProperty.get() == -1) "null" else nextProperty.get().toString()) +
+                (if (callbackProperty.get() != null && callbackProperty.get().isNotEmpty()) ",${callbackProperty.get()}" else "") +
+                "]"
+    }
+}
